2d7e276e12e60fa38bde04ed29ebb222a335f4ad
davimercatelli <davimercatelli@hotmail.com>
2020-06-18 20:59:19 +0000

    Blueprint and Configurations

---
+++ flaskblog/config.py
@@ -0,0 +1,23 @@
+#In ubuntu use 'nano .bashrc' to create Environments Variables:
+
+#export SECRET_KEY='your_secret_key'
+#export SQLALCHEMY_DATABASE_URI='your_path_folder'
+
+#export EMAIL_USER='your_user_email@gmail.com'
+#export EMAIL_PASSWORD='your_app_password'
+
+#Save file and restart terminal
+#For safety reasons it's necessary create an App Password in your Google Account
+
+
+import os
+
+class Config:
+
+	SECRET_KEY = os.environ.get('SECRET_KEY')
+	SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+	MAIL_SERVER = 'smtp.googlemail.com'
+	MAIL_PORT = 587
+	MAIL_USE_TLS = True
+	MAIL_USERNAME = os.environ.get('EMAIL_USER')
+	MAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
