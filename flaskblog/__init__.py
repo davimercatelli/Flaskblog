4b59d82cc83d668af31f8af5e1e23edf1d8989a9
davimercatelli <davimercatelli@hotmail.com>
2020-06-18 20:52:17 +0000

    Initialize app and extensions

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,37 +1,33 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail, Message
+from flaskblog.config import Config
 
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
+mail = Mail()
 
 
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
+def create_app(config_class=Config):
+	app = Flask(__name__)
+	app.config.from_object(Config)
 
-#For ubuntu use 'nano .bashrc' and create these Environments Variables:
+	db.init_app(app)
+	bcrypt.init_app(app)
+	login_manager.init_app(app)
+	mail.init_app(app)
 
-#export EMAIL_USER="your_user_email@gmail.com"
-#export EMAIL_PASSWORD="your_app_password"
+	from flaskblog.users.routes import users
+	from flaskblog.posts.routes import posts
+	from flaskblog.main.routes import main
 
-#Then restart the terminal
+	app.register_blueprint(users)
+	app.register_blueprint(posts)
+	app.register_blueprint(main)
 
-#To improve safety it's necessary create an App Password in your Google Account
-
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASSWORD')
-
-mail = Mail(app)
-
-from flaskblog import routes
+	return app
