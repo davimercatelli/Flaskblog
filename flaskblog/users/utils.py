4bcc79b823eb1afda096fff515855672ef03ad75
davimercatelli <davimercatelli@hotmail.com>
2020-06-21 18:51:56 +0000

    Layout Modifications

--- flaskblog/users/utils.py
+++ flaskblog/users/utils.py
@@ -11,7 +11,7 @@ def save_picture(form_picture):
 	_, f_ext = os.path.splitext(form_picture.filename)
 	picture_fn = random_hex + f_ext
 	picture_path = os.path.join(current_app.root_path, 'static/images', picture_fn)
-	output_size = (250, 250)
+	output_size = (500, 500)
 	i = Image.open(form_picture)
 	i.thumbnail(output_size)
 	i.save(picture_path)
